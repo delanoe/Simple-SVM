28f5d2115033fb0f69332ef8d5f140e3e9ac2243
Alexandre Delanoë <anoe@debian.org>
2017-06-04 06:50:39 +0200

    FIX for last version of libsvm

--- AI/SVM/Base_stub.h
+++ AI/SVM/Base_stub.h
@@ -2,7 +2,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern void AIziSVMziBase_deuI(StgStablePtr the_stableptr, HsPtr a1);
+extern void AIziSVMziBase_deuA(StgStablePtr the_stableptr, HsPtr a1);
 #ifdef __cplusplus
 }
 #endif
